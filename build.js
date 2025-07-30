8d9f2224f5b67abecd85e8d0a5bbd8a7f39e4538
MaxiEliteExecutor <maxielitecheat@gmail.com>
2025-07-30 09:05:47 +0200

    Update build.js

--- build.js
+++ build.js
@@ -5,6 +5,3 @@ html = html
   .replace("__GITHUB_TOKEN__", process.env.GITHUB_TOKEN)
   .replace("__GITHUB_USERNAME__", process.env.GITHUB_USERNAME)
   .replace("__GITHUB_REPO__", process.env.GITHUB_REPO);
-
-if (!fs.existsSync("public")) fs.mkdirSync("public");
-fs.writeFileSync("public/luashieldfree/create.html", html);
