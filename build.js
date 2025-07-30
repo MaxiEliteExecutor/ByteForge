fe5fbc0a9e2add70a19bae5ce679c606f11b38d3
MaxiEliteExecutor <maxielitecheat@gmail.com>
2025-07-30 09:06:11 +0200

    Delete build.js

--- build.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-let html = fs.readFileSync("luashieldfree/create.html", "utf8");
-html = html
-  .replace("__GITHUB_TOKEN__", process.env.GITHUB_TOKEN)
-  .replace("__GITHUB_USERNAME__", process.env.GITHUB_USERNAME)
-  .replace("__GITHUB_REPO__", process.env.GITHUB_REPO);
