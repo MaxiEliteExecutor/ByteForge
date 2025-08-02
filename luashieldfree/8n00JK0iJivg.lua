1bd53486d1220b43f83998e704940c5f4d9396c6
MaxiEliteExecutor <maxielitecheat@gmail.com>
2025-08-02 11:17:07 +0200

    Create luashieldfree/8n00JK0iJivg.lua

---
+++ luashieldfree/8n00JK0iJivg.lua
@@ -0,0 +1,6 @@
+local key = game:GetService("HttpService"):HttpGet("https://www.byteforge-getnow.space/luashieldfree/8n00JK0iJivg.lua.key")
+if key == "dshyfcmtmZwon5Hn" then
+[key_string=<string>] [text or file]
+else
+error("Invalid key")
+end
